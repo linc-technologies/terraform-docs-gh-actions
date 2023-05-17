0e7a0e326f70346599bc99e4a5054a33bb20570e
David King <david@linc-ed.com>
2023-05-18 09:17:34 +1200

    chore: remove bap script as Github now builds on ci run

--- bap.sh
+++
@@ -1,4 +0,0 @@
-docker build . -t linced.azurecr.io/tools/terraform-docs
-docker build . -t linced.azurecr.io/tools/terraform-docs:latest
-docker push linced.azurecr.io/tools/terraform-docs
-docker push linced.azurecr.io/tools/terraform-docs:latest
